b788f757f3d68e20d970836bbef1d4a9ade176a9
dyj132 <ydong51432@gmail.com>
2021-04-10 11:55:15 -0500

    Large-scale experiments update

--- main_CUR_largescale.m
+++ main_CUR_largescale.m
@@ -7,7 +7,7 @@ k = [10,10,10,10,20,40,60,80,100,120,140,160,180,200];
 tag = 'snn-1e6-1e6-a2b1-k100-r400-s2or';
 algos = {'CPQR','CPQR2pass','LUPP','LUPP2pass',...
          'RSVDDEIM','RSVDLS'};
-% test_CUR_large(1e6, k, algos, tag)
+test_CUR_large(1e6, k, algos, tag)
 
 %% load output data
 time = load(sprintf('time_%s.mat',tag));
